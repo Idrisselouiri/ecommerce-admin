81d258d4b991a988b11df5f0531bfcd892b89a69
ayoubtahir <121037450+Idrisselouiri@users.noreply.github>
2024-04-09 12:10:05 +0000

    nothing

--- app/firebase.js
+++ app/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC58A6gozt1uD76fX7DgCjnRLIjnjV8LJg",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern0blog.firebaseapp.com",
   projectId: "mern0blog",
   storageBucket: "mern0blog.appspot.com",
